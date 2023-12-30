22b2dacfa215de4a55acda8afc9f2d4ea2aa8bf0
Adamya Neupane <adamsneupane@gmail.com>
2023-12-30 20:30:52 +0545

    added project login and signup files

---
+++ My_App/My_App/src/my_app/My_App.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package my_app;
+import my_app.Login_Application;
+/**
+ *
+ * @author Aayash
+ */
+public class My_App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       var login = new  Login_Application();
+       login.setVisible(true);
+    }
+}
